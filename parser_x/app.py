68c8d0182fa15b6bd5d0bab8f933d36121806433
AleksGin <alexanderginin@icloud.com>
2025-01-24 00:26:50 +0300

    create main() func

---
+++ parser_x/app.py
@@ -0,0 +1,63 @@
+import asyncio
+from pathlib import Path
+
+from aiohttp import ClientSession
+from core import settings
+from database import (
+    create_tables,
+    db_session,
+)
+from repos import (
+    DataBaseRepository,
+    SpimexRepository,
+)
+from services import (
+    DownloadService,
+    ReadAndWriteService,
+)
+
+
+async def main():
+    await create_tables()
+    print("Таблицы успешно созданы!✅")
+
+    async with ClientSession() as http_session:
+        spimex_repo = SpimexRepository(
+            http_session=http_session,
+            results_url=settings.SPX_Config.results_url,
+            path_to_folder=Path(settings.SPX_Config.path_to_folder),
+            upload_url=settings.SPX_Config.upload_url,
+        )
+        print("SpimexRepo инициализирован ✅")
+        download_service = DownloadService(
+            upload_url=settings.SPX_Config.upload_url,
+            path_to_folder=Path(settings.SPX_Config.path_to_folder),
+            http_session=http_session,
+        )
+        print("DownloadService инициализирован ✅")
+        read_and_write_service = ReadAndWriteService(
+            path_to_folder=Path(settings.SPX_Config.path_to_folder),
+        )
+        print("ReadAndWrite сервис инициализирован ✅")
+        await asyncio.sleep(3)
+
+        files_to_download = await spimex_repo.get_latest_results()
+        print("Самый свежий файл скачен ✅")
+        await asyncio.sleep(3)
+
+        for file in files_to_download:
+            await download_service.download_file(file)
+
+        data = read_and_write_service.get_data_from_excel_file()
+        print(f"Обработано записей: {len(data)} ✅")
+        await asyncio.sleep(3)
+
+        if data:
+            print("Данные направлены✅")
+            async with db_session() as session:
+                db_repo = DataBaseRepository(session=session)
+                await db_repo.save_to_db(data)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
