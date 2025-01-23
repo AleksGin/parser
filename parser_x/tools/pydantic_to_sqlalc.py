0428bd9afb16d6ed0798f2cd7cc88808b17ace59
AleksGin <alexanderginin@icloud.com>
2025-01-24 00:59:57 +0300

    created pydantic_to_sqlalchemy func

---
+++ parser_x/tools/pydantic_to_sqlalc.py
@@ -0,0 +1,6 @@
+from schemas import ParseInfoSchema
+from models import SpimexTraidingResut
+
+
+def pydantic_to_sqlalchemy(pydantic_obj: ParseInfoSchema) -> SpimexTraidingResut:
+    return SpimexTraidingResut(**pydantic_obj.model_dump())
