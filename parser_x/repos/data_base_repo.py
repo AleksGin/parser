3a824fd8d6a8ff92abbc0522782d3f8a202e5f4d
AleksGin <alexanderginin@icloud.com>
2025-02-08 18:21:04 +0300

    create sync_app version of this parser (did it just for testcase)

--- parser_x/repos/data_base_repo.py
+++ parser_x/repos/data_base_repo.py
@@ -1,38 +1,43 @@
-import time
 from typing import List
 
 from models import SpimexTraidingResut
 from schemas import ParseInfoSchema
 from sqlalchemy import insert
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import Session
 from tools import pydantic_to_sqlalchemy
 
 
 class DataBaseRepository:
-    def __init__(self, session: AsyncSession):
-        self.session = session
+    def __init__(
+        self,
+        async_session: AsyncSession | None = None,
+        sync_session: Session | None = None,
+    ):
+        self.async_session = async_session
+        self.sync_session = sync_session
 
-    async def save_to_db(self, data: List[ParseInfoSchema]):
-        print("Данные получены✅")
+    async def async_save_to_db(self, data: List[ParseInfoSchema]):
         try:
-            start = time.time()
-            objects = [pydantic_to_sqlalchemy(pydantic_obj=item) for item in data]
-            await self.session.execute(
-                insert(SpimexTraidingResut), [obj.__dict__ for obj in objects]
-            )
-            await self.session.commit()
-            end = time.time()
-            print("Данные успешно добавлены в базу✅")
-            print(f"Время {end - start} секунд")
+            if self.async_session:
+                objects = [pydantic_to_sqlalchemy(pydantic_obj=item) for item in data]
+                await self.async_session.execute(
+                    insert(SpimexTraidingResut), [obj.__dict__ for obj in objects]
+                )
+                await self.async_session.commit()
+                print("Данные успешно добавлены в базу✅")
 
         except Exception as e:
             print(f"Ошибка при добавлении: {e}")
 
-
-# с циклом fore - Время 0.028119802474975586 секунд
-
-# с execute - Время 0.015104055404663086 секунд
-
-# с циклом for для 5 страниц - Время 0.7572510242462158 секунд
-
-# с execute - Время 0.38246703147888184 секунд
+    def sync_save_to_db(self, data: List[ParseInfoSchema]):
+        try:
+            if self.sync_session:
+                objects = [pydantic_to_sqlalchemy(pydantic_obj=item) for item in data]
+                self.sync_session.execute(
+                    insert(SpimexTraidingResut), [obj.__dict__ for obj in objects]
+                )
+                self.sync_session.commit()
+                print("Данные успешно добавлены в базу✅")
+        except Exception as e:
+            print(f"Ошибка при добавлении: {e}")
