33c9a885ace04460c676f225c191b6670231c585
AleksGin <alexanderginin@icloud.com>
2025-02-08 15:02:31 +0300

    added new services to export

--- parser_x/services/__init__.py
+++ parser_x/services/__init__.py
@@ -1,7 +1,11 @@
 __all__ = (
     "DownloadService",
     "ReadAndWriteService",
+    "SyncDownloadService",
+    "AsyncReadAndWriteService",
 )
 
-from .download_service import DownloadService
-from .read_service import ReadAndWriteService
+from .async_download_service import DownloadService
+from .async_read_service import AsyncReadAndWriteService
+from .sync_download_service import SyncDownloadService
+from .sync_read_service import ReadAndWriteService
