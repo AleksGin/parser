76a42bc51677d5dd825fa3fd4b2d6c7ea8adf398
AleksGin <alexanderginin@icloud.com>
2025-02-08 14:59:49 +0300

    query optimization

--- parser_x/repos/data_base_repo.py
+++ parser_x/repos/data_base_repo.py
@@ -1,6 +1,10 @@
+import time
 from typing import List
-from sqlalchemy.ext.asyncio import AsyncSession
+
+from models import SpimexTraidingResut
 from schemas import ParseInfoSchema
+from sqlalchemy import insert
+from sqlalchemy.ext.asyncio import AsyncSession
 from tools import pydantic_to_sqlalchemy
 
 
@@ -11,10 +15,24 @@ class DataBaseRepository:
     async def save_to_db(self, data: List[ParseInfoSchema]):
         print("Данные получены✅")
         try:
-            for item in data:
-                sqlalchemy_obj = pydantic_to_sqlalchemy(pydantic_obj=item)
-                self.session.add(sqlalchemy_obj)
+            start = time.time()
+            objects = [pydantic_to_sqlalchemy(pydantic_obj=item) for item in data]
+            await self.session.execute(
+                insert(SpimexTraidingResut), [obj.__dict__ for obj in objects]
+            )
             await self.session.commit()
+            end = time.time()
             print("Данные успешно добавлены в базу✅")
+            print(f"Время {end - start} секунд")
+
         except Exception as e:
             print(f"Ошибка при добавлении: {e}")
+
+
+# с циклом fore - Время 0.028119802474975586 секунд
+
+# с execute - Время 0.015104055404663086 секунд
+
+# с циклом for для 5 страниц - Время 0.7572510242462158 секунд
+
+# с execute - Время 0.38246703147888184 секунд
