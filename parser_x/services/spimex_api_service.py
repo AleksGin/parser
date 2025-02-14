3a6a4eafdd974354df4cd7d7e46fc2de64a409e2
AleksGin <alexanderginin@icloud.com>
2025-02-14 16:52:49 +0300

    added new methods to SpimexApiService

--- parser_x/services/spimex_api_service.py
+++ parser_x/services/spimex_api_service.py
@@ -5,5 +5,11 @@ class SpimexApiService:
     def __init__(self, db_repo: DataBaseRepository) -> None:
         self.db_repo = db_repo
 
-    
-    
+    async def get_last_trading_dates(self): ...
+
+    async def get_dynamics(self): ...
+
+    async def get_trading_results(self): ...
+
+
+
