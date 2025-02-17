a74411f4b211fc932263f1d70844e548a46c8636
AleksGin <alexanderginin@icloud.com>
2025-02-17 19:36:33 +0300

    deleted sync connect and rework DataBaseRepository

--- parser_x/repos/data_base_repo.py
+++ parser_x/repos/data_base_repo.py
@@ -4,18 +4,15 @@ from models import SpimexTraidingResut
 from schemas import ParseInfoSchema
 from sqlalchemy import insert
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.orm import Session
 from tools import pydantic_to_sqlalchemy
 
 
 class DataBaseRepository:
     def __init__(
         self,
-        async_session: AsyncSession | None = None,
-        sync_session: Session | None = None,
+        async_session: AsyncSession,
     ):
         self.async_session = async_session
-        self.sync_session = sync_session
 
     async def async_save_to_db(self, data: List[ParseInfoSchema]):
         try:
@@ -38,15 +35,3 @@ class DataBaseRepository:
 
     async def load_trading_results(self):
         pass
-
-    def sync_save_to_db(self, data: List[ParseInfoSchema]):
-        try:
-            if self.sync_session:
-                objects = [pydantic_to_sqlalchemy(pydantic_obj=item) for item in data]
-                self.sync_session.execute(
-                    insert(SpimexTraidingResut), [obj.__dict__ for obj in objects]
-                )
-                self.sync_session.commit()
-                print("Данные успешно добавлены в базу✅")
-        except Exception as e:
-            print(f"Ошибка при добавлении: {e}")
