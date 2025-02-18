6825b892813ba1cc7dc0518bed16d34f5d51d7b8
AleksGin <alexanderginin@icloud.com>
2025-02-18 18:47:22 +0300

    added SpimexApiService to export

--- parser_x/services/__init__.py
+++ parser_x/services/__init__.py
@@ -1,7 +1,9 @@
 __all__ = (
     "DownloadService",
     "AsyncReadAndWriteService",
+    "SpimexApiService"
 )
 
 from .download_service import DownloadService
 from .read_service import AsyncReadAndWriteService
+from .spimex_api_service import SpimexApiService
