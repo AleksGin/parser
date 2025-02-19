2b1db181441b6beb192aab88cebc27751be1d66f
AleksGin <alexanderginin@icloud.com>
2025-02-19 18:56:08 +0300

    added new schema to last_trading_dates endpoint

--- spimex_service/api/spimex.py
+++ spimex_service/api/spimex.py
@@ -4,7 +4,10 @@ from fastapi import (
     APIRouter,
     Depends,
 )
-from schemas import ParseInfoSchema
+from schemas import (
+    TradingDateSchema,
+    ParseInfoSchema,
+)
 from services import SpimexApiService
 
 from .dependencies import get_spimex_service
@@ -12,13 +15,13 @@ from .dependencies import get_spimex_service
 router = APIRouter(tags=["SpimexInfo"], prefix="/spimex_api_v1")
 
 
-@router.get("", response_model=list[ParseInfoSchema])
+@router.get("", response_model=list[TradingDateSchema])
 async def last_trading_dates(
     spimex_service: Annotated[
         SpimexApiService,
         Depends(get_spimex_service),
     ],
     count: int,
-) -> list[ParseInfoSchema]:
+) -> list[TradingDateSchema]:
     last_trading_dates = await spimex_service.get_last_trading_dates(count)
     return last_trading_dates
