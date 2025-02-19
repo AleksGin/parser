cb351a35298317f9edbdd79f4da9a1ed1efd4ab7
AleksGin <alexanderginin@icloud.com>
2025-02-19 14:58:45 +0300

    copy parser_info_schema.py from parser_x to spimex_service/schemas

---
+++ spimex_service/schemas/parse_info_schema.py
@@ -0,0 +1,43 @@
+from datetime import datetime
+from zoneinfo import ZoneInfo
+
+from pydantic import (
+    BaseModel,
+    ConfigDict,
+)
+
+MOSCOW_TZ = ZoneInfo("Europe/Moscow")
+
+
+def moscow_now() -> datetime:
+    return datetime.now(MOSCOW_TZ)
+
+
+class ParseInfoSchema(BaseModel):
+    exchange_product_id: str
+    exchange_product_name: str
+    oil_id: str
+    delivery_basis_id: str
+    delivery_basis_name: str
+    delivery_type_id: str
+    volume: float
+    total: float
+    count: int
+    date: str
+    created_on: str
+    updated_on: str
+
+    @property
+    def oil_id_getter(self) -> str:
+        return self.exchange_product_id[:4]
+
+    @property
+    def delivery_basis_id_getter(self) -> str:
+        return self.exchange_product_id[4:7]
+
+    @property
+    def delivery_type_id_getter(self) -> str:
+        return self.exchange_product_id[-1]
+    
+    model_config = ConfigDict(from_attributes=True)
+
