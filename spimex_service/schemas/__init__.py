2ea8ac2787250d25f959568c3238bc4638d59d56
AleksGin <alexanderginin@icloud.com>
2025-02-20 17:01:09 +0300

    added DynamicsDataSchema to export

--- spimex_service/schemas/__init__.py
+++ spimex_service/schemas/__init__.py
@@ -2,10 +2,12 @@ __all__ = (
     "ParseInfoSchema",
     "moscow_now",
     "TradingDateSchema",
+    "DynamicsDataSchema",
 )
 
 from .parse_info_schema import (
     ParseInfoSchema,
     moscow_now,
-    TradingDateSchema
+    TradingDateSchema,
+    DynamicsDataSchema,
 )
