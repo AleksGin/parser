d8d61a6b5a9ced4947765e9e007fa14e195be414
AleksGin <alexanderginin@icloud.com>
2025-02-20 17:01:41 +0300

    added new endpoint to spimex_api_v1

--- spimex_service/api/spimex.py
+++ spimex_service/api/spimex.py
@@ -4,7 +4,10 @@ from fastapi import (
     APIRouter,
     Depends,
 )
-from schemas import TradingDateSchema
+from schemas import (
+    TradingDateSchema,
+    DynamicsDataSchema,
+)
 from services import SpimexApiService
 
 from .dependencies import get_spimex_service
@@ -12,7 +15,7 @@ from .dependencies import get_spimex_service
 router = APIRouter(tags=["SpimexInfo"], prefix="/spimex_api_v1")
 
 
-@router.get("", response_model=list[TradingDateSchema])
+@router.get("/last_trading_dates", response_model=list[TradingDateSchema])
 async def last_trading_dates(
     spimex_service: Annotated[
         SpimexApiService,
@@ -22,3 +25,17 @@ async def last_trading_dates(
 ) -> list[TradingDateSchema]:
     last_trading_dates = await spimex_service.get_last_trading_dates(count)
     return last_trading_dates
+
+
+@router.get("/dynamics_data", response_model=list[DynamicsDataSchema])
+async def dynamics_data(
+    spimex_service: Annotated[
+        SpimexApiService,
+        Depends(get_spimex_service),
+    ],
+    start_date: str,
+    end_date: str,
+    oil_id: str,
+    delivery_typle_id: str,
+    delivery_basis_id: str,
+): ...
