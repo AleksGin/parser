610f965bcfae752ba0bf8ca1b605cb7817198832
AleksGin <alexanderginin@icloud.com>
2025-02-22 15:19:34 +0300

    added redis_helper to export

--- spimex_service/api/dependencies/__init__.py
+++ spimex_service/api/dependencies/__init__.py
@@ -1,6 +1,8 @@
 __all__ = (
     "get_spimex_service",
+    "redis_helper",
 )
 
 
-from .dependencies import get_spimex_service
+from .dependencies_repos import get_spimex_service
+from .redis_client import redis_helper
