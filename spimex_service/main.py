15b8b279faef9afa541dd1f5414b9aa034478f9f
AleksGin <alexanderginin@icloud.com>
2025-02-22 15:19:06 +0300

    added task to lifespan func

--- spimex_service/main.py
+++ spimex_service/main.py
@@ -1,7 +1,9 @@
+import asyncio
 from contextlib import asynccontextmanager
 
 import uvicorn
 from api import spimex_info_router
+from api.dependencies import redis_helper
 from core import settings
 from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
@@ -11,6 +13,7 @@ from models import db_helper
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # start
+    asyncio.create_task(redis_helper.schedule_cache_clear())
     yield
     # shutdown
     await db_helper.dispose()
@@ -20,6 +23,7 @@ app = FastAPI(
     lifespan=lifespan,
     default_response_class=ORJSONResponse,
 )
+
 
 app.include_router(spimex_info_router)
 
